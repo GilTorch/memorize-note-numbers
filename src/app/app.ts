b34a10edbe9ee63a05792e1a35cfec847bcf2067
GilTorch <infotorch2014@gmail.com>
2026-04-12 13:40:09 -0400

    introduce degrees-numbers-sheet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { SelectableNotesComponent } from './selectable-notes/selectable-notes.component';
+import { DegreesNumbersSheetComponent } from "./degrees-numbers-sheet/degrees-numbers-sheet.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [SelectableNotesComponent]
+  imports: [SelectableNotesComponent, DegreesNumbersSheetComponent]
 })
 export class App {
   protected readonly title = signal('memorize-note-numbers');
--- src/app/degrees-numbers-sheet/degrees-numbers-sheet.component.ts
+++ src/app/degrees-numbers-sheet/degrees-numbers-sheet.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, computed, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { KeysService } from '../keys.service';
 @Component({
   selector: 'app-degrees-numbers-sheet',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './degrees-numbers-sheet.component.html',
   styleUrl: './degrees-numbers-sheet.component.scss',
 })
-export class DegreesNumbersSheetComponent {}
+export class DegreesNumbersSheetComponent {
+  keysService = inject(KeysService);
+  degreesNumbers = this.keysService.keyDegreesToNumbers
+  degreesNumbersList = computed(() => {
+    return Object.entries(this.degreesNumbers())
+  })
+}

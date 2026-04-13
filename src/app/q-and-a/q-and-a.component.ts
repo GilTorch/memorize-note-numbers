238ca0fbac42b0e587a9a14f38dd93346fd0bacf
GilTorch <infotorch2014@gmail.com>
2026-04-13 01:51:12 -0400

    miscellaneous

--- src/app/q-and-a/q-and-a.component.ts
+++ src/app/q-and-a/q-and-a.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { DegreeKeyComponent } from '../degree-key/degree-key.component';
 import { SelectableComponent } from '../selectable/selectable.component';
 import { KeysService } from '../keys.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-q-and-a',
-  imports: [DegreeKeyComponent, SelectableComponent],
+  imports: [DegreeKeyComponent, SelectableComponent, MatIconModule],
   templateUrl: './q-and-a.component.html',
   styleUrl: './q-and-a.component.scss',
 })

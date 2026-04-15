c7a160521ac3927e559781de5c011ee2174d9724
GilTorch <infotorch2014@gmail.com>
2026-04-14 21:31:29 -0400

    fix type issues

--- src/app/degrees-keys-sheet/degrees-keys-sheet.component.ts
+++ src/app/degrees-keys-sheet/degrees-keys-sheet.component.ts
@@ -12,10 +12,12 @@ import { Degree, Key } from '../key.model';
 export class DegreesKeysSheetComponent {
   keysService = inject(KeysService);
   keysToDegrees = this.keysService.keysToDegrees
+  
   keysToDegreesList = computed(() => {
     if(this.keysToDegrees()){
-     return  Object.entries(this.keysToDegrees() as Record<Degree, Key>)
+     return  Object.entries(this.keysToDegrees() as Record<Degree, Key>) as unknown as [Degree, Key][]
     }
-    return []
+    return undefined
   })
+
 }
